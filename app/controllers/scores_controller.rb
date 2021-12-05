fd40d6b8be2ad765b4445e6e4f06e543323b5206
Kei <kt10195665@yahoo.ne.jp>
2021-12-05 16:45:53 +0900

    Add: とりあえず取得した緯度経度で道を表示させることができた

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -4,8 +4,10 @@ class ScoresController < ApplicationController
       [-33.8671, 151.20714],
       [-33.86708, 151.20683000000002],
       [-33.867070000000005, 151.20674000000002],
-      [-33.86703, 151.20625]
+      [-33.86703, 151.20625],
+      [-33.96703, 151.30625]
     ]
     @results = gmap.snap_to_roads(path, interpolate: true)
+    gon.json = @results
   end
 end
