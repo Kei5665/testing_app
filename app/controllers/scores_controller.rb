65ddd7ca8b548eadd1c3f6daf6d12eebef30eaf8
Kei <kt10195665@yahoo.ne.jp>
2021-12-05 10:47:27 +0900

    Add: viewとcontrollerの作成

---
+++ app/controllers/scores_controller.rb
@@ -0,0 +1,11 @@
+class ScoresController < ApplicationController
+  def index
+    path = [
+      [-33.8671, 151.20714],
+      [-33.86708, 151.20683000000002],
+      [-33.867070000000005, 151.20674000000002],
+      [-33.86703, 151.20625]
+    ]
+    @results = gmaps.snap_to_roads(path, interpolate: true)
+  end
+end
