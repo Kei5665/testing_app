2419a7a475594a9f72d36ba345c7a418307f8650
Kei <kt10195665@yahoo.ne.jp>
2021-12-09 18:48:26 +0900

    Add:　座標を保存するアクションを作成

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,22 @@
 class MapsController < ApplicationController
+  protect_from_forgery
   def index
   end
   def new
+  end
+  def create
+    json =  params[:data]
+    json_data = JSON.parse(json)
+
+    score = []
+    arr = []
+
+    json_data.each do |t|
+      arr = [t["lat"],t["lng"]]
+      score.push(arr)
+    end
+    encoded_path = GoogleMapsService::Polyline.encode(score)
+    map_data = Map.new(encorded_path: encoded_path)
+    map_data.save
   end
 end
